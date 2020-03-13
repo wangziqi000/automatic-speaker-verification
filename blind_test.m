c738dec55d1f3d0ab4737b2ef4ed641b3cd1c8de
Ziqi Wang <wangziqi000@126.com>
2020-03-12 21:26:21 -0700

    blind tests

--- blind_test.m
+++ blind_test.m
@@ -4,7 +4,8 @@ testList = 'test_read_trials.txt';
 blind_list = 'blind_file_list';
 blind_trials = 'blind_trials';
 
-
+use_pca = 0;
+pca_latent_knob = 0.99999;
 
 testScores = blind_nn(allFiles, trainList, testList, blind_list, ...
     blind_trials, use_pca, pca_latent_knob);
